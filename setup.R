ea9c1c43ff8017794a7468ea65a0225da16a764d
brunocr211 <brunocardosorodrigues01@gmail.com>
2026-05-01 11:37:12 -0300

    Add files via upload

---
+++ setup.R
@@ -0,0 +1,8 @@
+#!/usr/bin/env Rscript
+library(devtools); library(roxygen2)
+
+# root = "/home/alal/Desktop/code/1CausalInference/aipwML"
+# setwd(root)
+# create('aipwML') # only run once
+
+document()
